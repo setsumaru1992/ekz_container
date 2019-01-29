fc285dc52af75e1feff64febc22e9402c0794fca
setsumaru <setsumaru@moyamoya.space>
2019-01-30 00:34:15 +0900

    HTML要素をベタ書き

--- ekz/src/App.js
+++ ekz/src/App.js
@@ -10,21 +10,44 @@ class App extends Component {
           <tr>
             <td>
               今日食べたいもの
+              <button>開く</button>
               <div>
                 <table className="table table-hover table-striped">
                   <tbody>
                   <tr>
-                    <td>和食</td>
+                    <td>和食&emsp;&emsp;
+                      <span>Good</span>&emsp;
+                      <span>Bad</span>&emsp;
+                      <button>編集</button>&emsp;
+                      <button>削除</button>
+                    </td>
                   </tr>
                   <tr>
-                    <td>洋食</td>
+                    <td>洋食&emsp;&emsp;
+                      <span>Good</span>&emsp;
+                      <span>Bad</span>&emsp;
+                      <button>編集</button>&emsp;
+                      <button>削除</button></td>
                   </tr>
                   <tr>
-                    <td>中華</td>
+                    <td>中華&emsp;&emsp;
+                      <span>Good</span>&emsp;
+                      <span>Bad</span>&emsp;
+                      <button>編集</button>&emsp;
+                      <button>削除</button></td>
                   </tr>
           </tbody>
                 </table>
                 <button>選び直す</button>
+                <button>追加</button>
+                <br/>&emsp;
+                <div>
+                  <form action="">
+                    名前&emsp;<input type="text" name="" id=""/><br/>
+                    リンク<input type="text" name="" id=""/><br/>
+                    <button>追加</button>
+                  </form>
+                </div>
               </div>
             </td>
           </tr>
