50fa1aad9e668a75c57beb20d1c39547947398cc
setsumaru <setsumaru@moyamoya.space>
2019-02-17 01:33:32 +0900

    react-domを入れるとwarning.jsga

--- ekz/src/App.js
+++ ekz/src/App.js
@@ -1,29 +1,29 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom"
+// import { BrowserRouter as Router, Route, Link } from "react-router-dom"
 import ThemeShow from '~/views/pages/themes/show'
 
 class App extends Component {
   render() {
     // return (
-      {/*<Router>*/}
-        {/*<ul>*/}
-          {/*<li>*/}
-            {/*<Link to="/">Home</Link>*/}
-          {/*</li>*/}
-          {/*<li>*/}
-            {/*<Link to="/about">About</Link>*/}
-          {/*</li>*/}
-          {/*<li>*/}
-            {/*<Link to="/topics">Topics</Link>*/}
-          {/*</li>*/}
-
-          {/*<hr />*/}
-
-          {/*/!*<Route exact path="/" component={<ThemeShow/>} />*!/*/}
-          {/*<Route path="/about" component={About} />*/}
-          {/*<Route path="/topics" component={Topics} />*/}
-        {/*</ul>*/}
-      {/*</Router>*/}
+    //   <Router>
+    //     <ul>
+    //       <li>
+    //         <Link to="/">Home</Link>
+    //       </li>
+    //       <li>
+    //         <Link to="/about">About</Link>
+    //       </li>
+    //       <li>
+    //         <Link to="/topics">Topics</Link>
+    //       </li>
+    //
+    //       <hr />
+    //
+    //       {/*<Route exact path="/" component={<ThemeShow/>} />*/}
+    //       <Route path="/about" component={About} />
+    //       <Route path="/topics" component={Topics} />
+    //     </ul>
+    //   </Router>
     // );
     return (
 
