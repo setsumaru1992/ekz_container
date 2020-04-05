05d827ac6fdbcb5417853a564a1e87a19cb50d81
setsumaru <setsumaru@moyamoya.space>
2020-04-05 14:44:54 +0900

    entrypointのバグ修正

--- entrypoint/entrypoint-ekz.sh
+++ entrypoint/entrypoint-ekz.sh
@@ -42,7 +42,7 @@ killProcess () {
     port=$1
     headerSample="PID"
     pid=$(lsof -i :$port | grep -v $headerSample | head -n 1 | awk '{print $2}')
-    if [ "$pid" <> "" ]; then
+    if [ ! "$pid" = "" ]; then
         execCmd="kill -9 $pid"
         echo "$execCmd"
         bash -c "$execCmd"
