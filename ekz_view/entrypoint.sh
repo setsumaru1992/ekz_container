b23c138116e68649415f4de8923a6ea4a7aec368
setsumaru1992 <setsumaru@muriyari.work>
2020-05-14 15:18:44 +0900

    master typescriptとreact-native-webを入れる

--- ekz_view/entrypoint.sh
+++ ekz_view/entrypoint.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
-yarn install
-port=18072
-yarn dev -p ${port}
+# yarn install
+# port=18072
+# yarn dev -p ${port}
+
+# 通常コマンドが失敗するときにコンテナを延命させるデバッグコマンド
+tail -n 1 -f package.json
---
+++ ekz_view/pages/index.tsx
@@ -0,0 +1,43 @@
+import { StyleSheet, Text, View } from 'react-native';
+import { withApollo } from '../lib/apollo';
+
+const IndexPage = () => (
+  <View style={styles.container}>
+    <Text accessibilityRole="header" style={styles.text}>
+      React Native for Web & Next.js
+    </Text>
+
+    {/* <Text style={styles.link} accessibilityRole="link" href="https://google.com"> */}
+    <Text style={styles.link} >
+      A universal link
+    </Text>
+
+    <View style={styles.textContainer}>
+      <Text accessibilityRole="header" aria-level="2" style={styles.text}>
+        Subheader
+      </Text>
+    </View>
+  </View>
+)
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    flexGrow: 1,
+    justifyContent: 'center',
+  },
+  link: {
+    color: 'blue',
+  },
+  textContainer: {
+    alignItems: 'center',
+    marginTop: 16,
+  },
+  text: {
+    alignItems: 'center',
+    fontSize: 24,
+    marginBottom: 24,
+  },
+})
+
+export default withApollo({ ssr: true })(IndexPage)
