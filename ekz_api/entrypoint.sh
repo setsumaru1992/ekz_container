d2930c5082536e72555586ce5f3da8370673ae78
Takahiro Umeda <takahiro.umeda@pixta.co.jp>
2022-07-10 09:03:16 +0900

    M1Macでアプリ起動ができるよう修正

--- ekz_api/entrypoint.sh
+++ ekz_api/entrypoint.sh
@@ -10,7 +10,7 @@ port=${EKZ_API_PORT}
 pre_process="$(ps -ef |grep ":$port" |grep -v "grep" |awk '{print $2}')"
 kill -9 $pre_process
 RAILS_ENV=development
-RAILS_ENV=${RAILS_ENV} ./bin/rails assets:precompile
+# RAILS_ENV=${RAILS_ENV} ./bin/rails assets:precompile # M1Macで動かなかったから、graphiql以外のviewをいじるまでコメントアウト
 RAILS_ENV=${RAILS_ENV} bundle exec rails s -p $port -b "0.0.0.0" &
 
 logfile="log/development.log"
